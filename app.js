3365160714229d7122211d7cbeb6d64d4b8e9853
Nico Herlim <nicoherlim2003@gmail.com>
2024-06-15 14:41:03 +0700

    using .env instead

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
-const { commandsHandler, eventsHandler } = require('./handler.js');
+const { Client, GatewayIntentBits } = require("discord.js");
+const { commandsHandler, eventsHandler } = require("./handler.js");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const token = process.env.token;
 
 // Create a new client instance
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-	],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 commandsHandler(client);
 eventsHandler(client);
 
-client.on('unhandledRejection', error => {
-	console.error('Unhandled promise rejection:', error);
+client.on("unhandledRejection", (error) => {
+  console.error("Unhandled promise rejection:", error);
 });
 
 // Log in to Discord with your client's token
-client.login(token)
-	.then(() => {
-		client.user.setPresence(
-			{
-				activities: [{ name: 'with discord.js' }],
-				status: 'idle',
-			},
-		);
-	})
-	.catch((error) => {
-		console.error('Error logging in:', error);
-	});
+client
+  .login(token)
+  .then(() => {
+    client.user.setPresence({
+      activities: [{ name: "with discord.js" }],
+      status: "idle",
+    });
+  })
+  .catch((error) => {
+    console.error("Error logging in:", error);
+  });
